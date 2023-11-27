b6222630f97cdb095538e898a2dd6e80e880a296
lore1379 <lorenzo.g92@gmail.com>
2023-11-27 16:43:12 +0100

    Implemented fantasy page with sample data

--- src/app/fantasy/fantasy.page.ts
+++ src/app/fantasy/fantasy.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonModal } from '@ionic/angular';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-fantasy',
@@ -9,11 +11,70 @@ import { OAuthService } from 'angular-oauth2-oidc';
 })
 export class FantasyPage implements OnInit {
 
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message!: string;
+  
   userName: string = "";
+
+  selectedSegment: string = 'fantasy'; // Default segment
+
+  selectedRole: string = 'gk'; // Default role
+
+  // sample data to try layouts
+  data: any[] = [
+    { playerName: 'Gabriele', playerScore: 6},
+    { playerName: 'Damiano', playerScore: 6},
+    { playerName: 'Lorenzo', playerScore: 9},
+    { playerName: 'Mich', playerScore: 2},
+    { playerName: 'Elias', playerScore: 6},
+  ];
+
+  // Sample array of goalkeepers
+  freeGoalkeepers: any[] = [
+    { name: 'GoalKeeper 1', role: '(GK)' },
+    { name: 'GoalKeeper 2', role: '(GK)' },
+    { name: 'GoalKeeper 3', role: '(GK)' },
+  ];
+
+  inTeamGoalkeepers: any[] = [
+    { name: 'prova' },
+  ]
+
+  selectedGoalkeeper: any;
 
   constructor(private oauthService: OAuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  cancel() {
+    console.log('Cancel button clicked');
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.selectedGoalkeeper, 'confirm');
+    console.log(this.selectedGoalkeeper);
+    this.inTeamGoalkeepers.push(this.selectedGoalkeeper);
+  }
+
+  cancel1() {
+    console.log('Cancel button clicked');
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm1() {
+    this.modal.dismiss(this.selectedGoalkeeper, 'confirm');
+    console.log(this.selectedGoalkeeper);
+    this.inTeamGoalkeepers.push(this.selectedGoalkeeper);
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
   }
 
   logout() {
